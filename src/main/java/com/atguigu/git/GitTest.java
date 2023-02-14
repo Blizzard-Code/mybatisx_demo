953ae080328051e1d893e573ff1a6150c6ee0af6
zhonghao <849826920@qq.com>
2023-02-14 11:34:27 +0800

    hot fix version2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,8 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git222");
         System.out.println("hello git33dasdasasda3");
+        System.out.println("hello git33dasdasasda3");
+        System.out.println("hello git33dasdasasda3");
+        System.out.println("hello git33dasdasasda3");
     }
 }
