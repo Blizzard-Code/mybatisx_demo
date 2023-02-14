427355c26918324640ad551e2785c95a8ff4f3be
Blizzard <46274949+Blizzard-Code@users.noreply.github.com>
2023-02-14 14:53:33 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,6 +10,6 @@ public class GitTest {
         System.out.println("hello git222");
         System.out.println("hello git33444444443");
         System.out.println("hello git33dasdasasda3");
-        System.out.println("push");
+        System.out.println("pull");
     }
 }
